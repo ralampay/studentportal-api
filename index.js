1b3ccbdf24163e1114cec1f383a10ee4bdac5a8a
ralampay <raphael.alampay@gmail.com>
2024-07-12 09:47:51 +0800

    checkpoint

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello world!");
+// Dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { Sequelize, Model, DataTypes } = require('sequelize');
+
+// Server Configuration
+
+// PORT: Where the server app will run (default: 3000)
+const port = 3000;
+const app = express();
+
+// MIDDLEWARE
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
