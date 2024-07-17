e6660417ad94209ac7104f13e2a012715c76e7e8
ralampay <raphael.alampay@gmail.com>
2024-07-17 10:18:00 +0800

    checkpoint

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const ValidateLogin = require('../operations/users/ValidateLogin');
+const { generateToken } = require('../helpers/AppHelper');
 
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
     console.log("POST /login");
 
-    res.json({
-        token: "1234",
-        user: {
-            username: "admin",
-            role: "admin"
+    const username = req.body.username;
+    const password = req.body.password;
+
+    let result = await ValidateLogin(username, password);
+
+    if (result.isValid) {
+        // return the token and user object
+        let user = {
+            username: result.user.username,
+            role: result.user.role
         }
-    });
+
+        let token = generateToken(user);
+
+        res.json({
+            token,
+            user
+        })
+    } else {
+        res.status(422).json(result.payload)
+    }
 })
 
 module.exports = router;
---
+++ src/operations/users/ValidateLogin.js
@@ -0,0 +1,47 @@
+const User = require('../../models/User');
+const { isValidLogin } = require('../../helpers/AppHelper');
+
+const ValidateLogin = async (username, password) => {
+    let isValid = true;
+    let user = null;
+
+    let payload = {
+        username: [],
+        password: []
+    }
+
+    if (!username) {
+        payload.username.push("required");
+    }
+
+    if (!password) {
+        payload.password.push("required");
+    }
+
+    if (username && password) {
+        user = await User.findOne({ username: username })
+
+        if (user) {
+            let isValid = await isValidLogin(password, user.passwordHash);
+
+            if (!isValid) {
+                payload.password.push("invalid password");
+            }
+        } else {
+            payload.username.push("not found");
+        }
+    }
+
+    // Loop through each key checking if we have error messages
+    Object.keys(payload).forEach((key) => {
+        if (payload[key].length > 0) {
+            isValid = false;
+        }
+    })
+
+    return {
+        isValid,
+        payload,
+        user
+    }
+}
