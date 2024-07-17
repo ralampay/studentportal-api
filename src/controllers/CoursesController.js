8db95c18d0a3e02bd00917b1392ea75e569d6dde
ralampay <raphael.alampay@gmail.com>
2024-07-17 12:06:52 +0800

    checkpoint

--- src/controllers/CoursesController.js
+++ src/controllers/CoursesController.js
@@ -2,8 +2,16 @@ const express = require('express');
 const Course = require('../models/Course');
 const ValidateCreate = require('../operations/courses/ValidateCreate');
 const router = express.Router();
+const jwt = require('jsonwebtoken')
 
 router.get("/courses", async (req, res) => {
+    console.log("Headers:")
+    console.log(req.headers)
+    let token = req.headers['authorization'].split(' ')[1]
+    console.log(`token: ${token}`)
+    let user = jwt.verify(token, 'secret');
+    console.log('User:')
+    console.log(user)
     let courses = await Course.findAll();
 
     res.json(courses);
