0aa8199d333438aeaa8105eeaaf7e90e23521943
ralampay <raphael.alampay@gmail.com>
2024-07-17 09:45:37 +0800

    checkpoint

--- tasks/bcryptExample.js
+++ tasks/bcryptExample.js
@@ -1,22 +1,33 @@
+/*
+Examples:
+1. getSalt(int): Create a proper salt value. int - Number of iterations.
+2. hash(plaintText, salt): Returns the encrypted version of plainText
+3. compare(plainText, hash): Returns boolean corresponding to correctness
+*/
 const bcrypt = require("bcrypt");
 
-let mySalt = '';
-bcrypt.genSalt(10, (err, salt) => {
-    mySalt = salt
-    console.log(`salt: ${mySalt}`)
-    
-    let password = 'mypassword'
+let salt = "";
+let password = "password";
 
-    let myHash = ""
-    
-    bcrypt.hash(password, mySalt).then((hash) => {
-        myHash = hash
-        console.log(`hash: ${myHash}`)
+const runExample = async () => {
+    salt = await bcrypt.genSalt(10);
+    console.log(`salt: ${salt}`);
 
-        let inputPassword = 'mypassword'
+    let hash = await bcrypt.hash(password, salt);
+    console.log(`hash: ${hash}`);
 
-        bcrypt.compare(inputPassword, myHash).then((result) => {
-            console.log(`result: ${result}`)
-        })
-    })
+    let inputPassword = "password";
+    // uses salt:
+    // let inputHash = await bcrypt.hash(inputPassword, salt);
+
+    // uses iteration count of bcrypt salt generation
+    let inputHash = await bcrypt.hash(inputPassword, 10);
+    console.log(`inputHash: ${inputHash}`);
+
+    let result = await bcrypt.compare(inputPassword, hash);
+    console.log(`result: ${result}`);
+}
+
+runExample().then(() => {
+    console.log("Done");
 })
